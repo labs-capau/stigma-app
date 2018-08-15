6d0be7e533b28dde6d89aaee4156c005b44a5c5d
HenriqueAlvesXavier <henry.alvyn.ha@gmail.com>
2018-08-15 14:59:16 -0300

    melhora layout da tela de avaliação, atualiza toast para vermelho quando der erro e verde quando der sucesso na página de avaliação.

--- src/pages/avaliacao/avaliacao.ts
+++ src/pages/avaliacao/avaliacao.ts
@@ -9,27 +9,29 @@ import { AvaliacaoProvider } from '../../providers/avaliacao/avaliacao';
   providers: [AvaliacaoProvider]
 })
 export class AvaliacaoPage {
-    
+
   avaliacao;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, 
+  constructor(public navCtrl: NavController, public navParams: NavParams,
     public toastCtrl: ToastController, public avaliacaoProvider: AvaliacaoProvider) {
     this.avaliacaoProvider.next(0,0).then((avaliacao) => {
       this.avaliacao = avaliacao;
     });
   }
-  
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad AvaliacaoPage');
   }
-  
+
   concluir() {
     this.avaliacaoProvider.save(this.avaliacao).then(() => {
 
       let toast = this.toastCtrl.create({
+        cssClass: "sucesso",
         message: 'Aluno avaliado com sucesso!',
         duration: 2000,
         position: "bottom"
+
       });
       toast.present(toast);
 
@@ -39,19 +41,21 @@ export class AvaliacaoPage {
 
     }).catch(() => {
       let toast = this.toastCtrl.create({
+        cssClass: "erro",
         message: 'Erro ao avaliar aluno!',
         duration: 2000,
         position: "bottom"
+
       });
       toast.present(toast);
-      
+
     });
-   
+
   }
 
   starClicked(criterio, nota) {
     console.log("Critério: " + criterio.descricao + " | Rated : " + nota);
     criterio.nota = nota;
   }
-  
+
 }
