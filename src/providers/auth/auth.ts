646de50647ce9ec27efb54da6b99b3990539f889
Ewerton Costa <ewertonsjp@gmail.com>
2018-12-12 18:58:16 -0300

    Adiciona turma_id aos dados de turma

--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -19,11 +19,13 @@ export class AuthProvider {
       {
         'turmas':[
           {
+            turma_id:1,
             descricao:'1º Mod em INFO',
             curso:'Informática para Internet',
             avls_pendentes:5
           },
           {
+            turma_id:2,
             descricao:'1º Ano em AGRO',
             curso:'Integrado em AGRO',
             avls_pendentes:10
